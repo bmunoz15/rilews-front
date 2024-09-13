c3e762e686331cfc68bc47e6072e66bbc61fa2d7
Bayron <b.munoz15@ufromail.cl>
2024-09-13 13:04:42 -0300

    feat(monitoring-system): añade HomeMs para encapsular la funcion de monitoreo de estaciones

---
+++ src/component/monitoring-system/home/HomeMs.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+import Map from '../../lib/map/Map';
+
+
+const Home: React.FC = () => {
+    return (
+        <>
+            <Map />
+        </>
+    );
+};
+
+export default Home;
