34eb2806d288648a74569dc15515891e85cc91ed
Bayron Muñoz <b.munoz15@ufromail.cl>
2024-09-13 01:28:31 -0300

    feat(User.tsx): anade boton para logear al usuario

--- src/App.tsx
+++ src/App.tsx
@@ -3,10 +3,12 @@ import Map from './component/map/Map'
 import 'leaflet/dist/leaflet.css'
 import Sidebar from './component/sidebar/Sidebar'
 import Footer from './component/footer/Footer'
+import User from './component/user/User'
 function App() {
   return (
     <>
       <Sidebar />
+      <User />
       <Map />
       <Footer />
     </>
