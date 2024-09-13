3893b459f8928b8e0a0131676e9dd59fb3b204c7
Bayron <b.munoz15@ufromail.cl>
2024-09-13 13:02:07 -0300

    feat(app.tsx): añade react-router y actualiza app.tsx para gestionar las rutas

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,27 @@
 import './App.css'
-import Map from './component/map/Map'
 import 'leaflet/dist/leaflet.css'
-import Sidebar from './component/sidebar/Sidebar'
+import { Route, BrowserRouter as Router, Routes } from 'react-router-dom'
+import HomeEws from './component/early-warning-system/home/HomeEws'
+import HomeMs from './component/monitoring-system/home/HomeMs'
+import Sidebar from './component/lib/sidebar/Sidebar'
 import Footer from './component/footer/Footer'
-import User from './component/user/User'
-import InformationPanel  from './component/warningview/InformationPanel '
+import User from './component/lib/user/User'
+import NotFound from './component/lib/not-found/NotFound'
 
 function App() {
   return (
     <>
-      <Sidebar />
-      <InformationPanel />
-      <User />
-      <Map />
-      <Footer />
+      <Router>
+        <Sidebar />
+        <User />
+        <Routes>
+          <Route path="/" element={<HomeEws />} />
+          <Route path="/monitoring-system" element={<HomeMs />} />
+          <Route path="*" element={<NotFound />} />
+        </Routes>
+        <Footer />
+
+      </Router>
     </>
   )
 }
