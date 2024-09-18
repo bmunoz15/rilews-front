e0917431e1eaba4232dbaa14ac33198ae9381708
Bayron <b.munoz15@ufromail.cl>
2024-09-18 00:18:08 -0300

    feat(mshomepage): añade mapprovider para manejar contexto global del mapa en modulo de monitoring-system

--- src/component/monitoring-system/view/MsHomePage.tsx
+++ src/component/monitoring-system/view/MsHomePage.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 
 import SharedMap from '../../shared/map/SharedMap';
 import { LatLngExpression } from 'leaflet';
+import { MapProvider } from '../../early-warning-system/context/MapProvider';
 
 const centerPosition: LatLngExpression = [-38.6679, -72.2610];
 const tileUrl = "https://tiles.stadiamaps.com/tiles/alidade_satellite/{z}/{x}/{y}{r}.png?api_key=bbaa0e7d-9c63-4ece-9a14-a60e6a430e23";
@@ -10,13 +11,16 @@ const tileAttribution = '&copy; CNES, Distribution Airbus DS, © Airbus DS, © P
 const Home: React.FC = () => {
     return (
         <>
-            <SharedMap
-                centerPosition={centerPosition}
-                zoom={7}
-                tileUrl={tileUrl}
-                tileAttribution={tileAttribution} children={undefined}            >
-                {/*añadir componentes (layers) aquí*/}
-            </SharedMap>
+            <MapProvider>
+                <SharedMap
+                    centerPosition={centerPosition}
+                    zoom={7}
+                    tileUrl={tileUrl}
+                    tileAttribution={tileAttribution} children={undefined}            >
+                    {/*añadir componentes (layers) aquí*/}
+                </SharedMap>
+            </MapProvider>
+
         </>
     );
 };
