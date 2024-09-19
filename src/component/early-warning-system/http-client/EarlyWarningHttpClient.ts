56351f1d0cc86e381915345adc08554edace1ab8
Bayron <b.munoz15@ufromail.cl>
2024-09-18 21:57:10 -0300

    refactor(earlywarninghttpclient): refactoriza la url para conexión con API

--- src/component/early-warning-system/http-client/EarlyWarningHttpClient.ts
+++ src/component/early-warning-system/http-client/EarlyWarningHttpClient.ts
@@ -1,11 +1,7 @@
 import axios from 'axios';
 
 const EarlyWarningHttpClient = axios.create({
-<<<<<<< HEAD
-    baseURL: 'http://192.168.249.30:8080/earlywarning',
-=======
     baseURL: 'http://localhost:8080/earlywarning',
->>>>>>> 635462cbf1f76c4d807b103d755a651cadd43e8e
     headers: {
         'Content-Type': 'application/json',
     },
