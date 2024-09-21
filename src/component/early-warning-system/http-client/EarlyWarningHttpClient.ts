568d8f534255bc44d8b0725625320d3709b0b9d2
Bayron <b.munoz15@ufromail.cl>
2024-09-21 00:51:05 -0300

    refactor(geojsoncprovider): refactoriza función para incluir nuevos atributos en geojsontype. Además, se actualiza la url de llamada a la api

--- src/component/early-warning-system/http-client/EarlyWarningHttpClient.ts
+++ src/component/early-warning-system/http-client/EarlyWarningHttpClient.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const EarlyWarningHttpClient = axios.create({
-    baseURL: 'http://localhost:8080/earlywarning',
+    baseURL: 'http://localhost:8080/api/earlywarning',
     headers: {
         'Content-Type': 'application/json',
     },
