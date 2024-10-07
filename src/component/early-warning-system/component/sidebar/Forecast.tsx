dcd1446bb09cd2dbfd8c5a4ad32e00dca19b58f8
Bayron <b.munoz15@ufromail.cl>
2024-10-07 16:39:40 -0300

    refactor(sidebar): refactoriza componentes para quitar responsividad y eliminar componente no utilizado

--- src/component/early-warning-system/component/sidebar/Forecast.tsx
+++ src/component/early-warning-system/component/sidebar/Forecast.tsx
@@ -1,144 +1,115 @@
 import React, { useState, useEffect, useRef } from 'react';
-import { Typography, Paper, Box, useTheme, useMediaQuery, IconButton, CircularProgress } from '@mui/material';
-import CloudIcon from '@mui/icons-material/Cloud';
+import { Typography, Paper, Box, CircularProgress } from '@mui/material';
 import ErrorOutlineIcon from '@mui/icons-material/ErrorOutline';
-import { getForecastDates } from '../../service/EarlyWarningService';
+import { getForecastDates, getAlerts } from '../../service/EarlyWarningService';
 import { ForecastModel } from '../../model/ForecastModel';
+import { useAlerts } from '../../context/GeoJsonProvider';
+import { format, parse } from 'date-fns';
 
-interface ForecastProps {
-    onPeriodSelect: (forecast: ForecastModel) => void;
-}
-
-const Forecast: React.FC<ForecastProps> = ({ onPeriodSelect }) => {
-    const [forecastModel, setData] = useState<ForecastModel[] | null>(null);
+const Forecast: React.FC = () => {
+    const { setAlerts } = useAlerts();
+    const [, setSelectedPeriod] = useState<ForecastModel>({
+        forecastDate: new Date().toISOString().split('T')[0].replace(/-/g, ''),
+        url: 'today',
+        period: '24h'
+    });
+    const [forecastModel, setForecastModel] = useState<ForecastModel[]>([]);
     const [loading, setLoading] = useState(true);
     const [error, setError] = useState<string | null>(null);
-
-    const theme = useTheme();
-    const isSmallScreen = useMediaQuery(theme.breakpoints.down('sm'));
-    const [expanded, setExpanded] = useState(false);
     const containerRef = useRef<HTMLDivElement>(null);
-
-    const handleIconClick = () => {
-        if (isSmallScreen) {
-            setExpanded(!expanded);
-        }
-    };
-
-    const handleClickOutside = (event: MouseEvent) => {
-        if (containerRef.current && !containerRef.current.contains(event.target as Node)) {
-            setExpanded(false);
-        }
-    };
-
-    const todayDate = () => {
-        return new Date().toISOString().split('T')[0].replace(/-/g, '');
-    };
-
-    useEffect(() => {
-        if (isSmallScreen) {
-            document.addEventListener('mousedown', handleClickOutside);
-        }
-        return () => {
-            document.removeEventListener('mousedown', handleClickOutside);
-        };
-    }, [isSmallScreen]);
 
     useEffect(() => {
         const fetchData = async () => {
             try {
-                const result = await getForecastDates(todayDate());
-                setData(result);
+                const today = new Date().toISOString().split('T')[0].replace(/-/g, '');
+                const result = await getForecastDates(today);
+                setForecastModel(result);
+                setAlerts(await getAlerts(today, 'today'));
             } catch (err) {
-                setError('Failed to fetch data');
+                setError('Failed to fetch forecast data');
             } finally {
                 setLoading(false);
             }
         };
+
         fetchData();
     }, []);
+    const handlePeriodSelect = async (props: ForecastModel) => {
+        const today = new Date().toISOString().split('T')[0].replace(/-/g, '');
+        const alertData = await getAlerts(today, props.url);
+        setAlerts(alertData);
+        setSelectedPeriod(props);
+    };
 
-
-    function formatForecastDate(forecastDate: string): React.ReactNode {
+    const formatForecastDate = (forecastDate: string) => {
         const forecastDateStr = forecastDate.toString();
-        const year = forecastDateStr.slice(0, 4);
-        const month = forecastDateStr.slice(4, 6);
-        const day = forecastDateStr.slice(6, 8);
-        return `${year}-${month}-${day}`;
-    }
+        if (forecastDateStr.length !== 8) {
+            return 'Invalid date';
+        }
+        const parsedDate = parse(forecastDateStr, 'yyyyMMdd', new Date());
+        if (isNaN(parsedDate.getTime())) {
+            return 'Invalid date';
+        }
+        return format(parsedDate, 'yyyy-MM-dd');
+    };
 
     return (
         <Box ref={containerRef}>
-            {isSmallScreen && !expanded && (
-                <Box display="flex" justifyContent="center" bgcolor="white" borderRadius={1}>
-                    <IconButton onClick={handleIconClick}>
-                        <CloudIcon style={{ fontSize: 24 }} />
-                    </IconButton>
-                </Box>
-            )}
-            {(expanded || !isSmallScreen) && (
-                <Paper
-                    style={{
-                        padding: 8,
-                        textAlign: 'center',
-                        borderRadius: 8,
-                        backgroundColor: theme.palette.background.default,
-                        transition: 'all 0.3s ease',
-                    }}
-                >
-                    {loading ? (
-                        <Box display="flex" flexDirection="column" alignItems="center">
-                            <CircularProgress />
-                            <Typography variant="subtitle2" style={{ marginTop: 8 }}>
-                                Loading...
-                            </Typography>
+            <Paper
+                style={{
+                    padding: 8,
+                    textAlign: 'center',
+                    borderRadius: 8,
+                    transition: 'all 0.3s ease',
+                }}
+            >
+                {loading ? (
+                    <Box display="flex" flexDirection="column" alignItems="center">
+                        <CircularProgress />
+                        <Typography variant="subtitle2" style={{ marginTop: 8 }}>
+                            Loading...
+                        </Typography>
+                    </Box>
+                ) : error ? (
+                    <Box display="flex" flexDirection="column" alignItems="center">
+                        <ErrorOutlineIcon color="error" />
+                        <Typography variant="subtitle2" style={{ marginTop: 8, color: 'red' }}>
+                            {error}
+                        </Typography>
+                    </Box>
+                ) : (
+                    <>
+                        <Typography variant="subtitle2" gutterBottom style={{ fontWeight: 'bold' }}>
+                            Fecha Pronóstico
+                        </Typography>
+                        <Box display="flex" justifyContent="space-between" gap={1}>
+                            {forecastModel.map((forecast, index) => (
+                                <Box
+                                    key={index}
+                                    textAlign="center"
+                                    flex={1}
+                                    sx={{
+                                        '&:hover': {
+                                            backgroundColor: 'lightgray',
+                                        },
+                                        '&:active': { backgroundColor: 'gray' },
+                                        borderRadius: 2,
+                                        cursor: 'pointer',
+                                    }}
+                                    onClick={() => handlePeriodSelect(forecast)}
+                                >
+                                    <Typography variant="subtitle2" style={{ fontWeight: 'bold' }}>
+                                        {forecast.period}
+                                    </Typography>
+                                    <Typography variant="body2">
+                                        {formatForecastDate(forecast.forecastDate)}
+                                    </Typography>
+                                </Box>
+                            ))}
                         </Box>
-                    ) : error ? (
-                        <Box display="flex" flexDirection="column" alignItems="center">
-                            <ErrorOutlineIcon color="error" />
-                            <Typography variant="subtitle2" style={{ marginTop: 8, color: theme.palette.error.main }}>
-                                {error}
-                            </Typography>
-                        </Box>
-                    ) : (
-                        <>
-                            <Typography variant="subtitle2" gutterBottom style={{ fontWeight: 'bold' }}>
-                                Fecha Pronóstico
-                            </Typography>
-                            <Box display="flex" justifyContent="space-between" width={"100%"} gap={1}>
-                                {forecastModel?.map((forecast, index) => (
-                                    <Box
-                                        key={index}
-                                        textAlign="center"
-                                        flex={1}
-                                        gap={1}
-                                        sx={{
-                                            '&:hover': {
-                                                backgroundColor: theme.palette.action.hover,
-
-                                            },
-                                            '&:active': { backgroundColor: theme.palette.action.selected },
-                                            borderRadius: 2,
-                                        }}
-
-                                        style={{ cursor: 'pointer' }}
-                                        onClick={() => {
-                                            onPeriodSelect(forecast);
-                                        }}
-                                    >
-                                        <Typography variant="subtitle2" style={{ fontWeight: 'bold' }}>
-                                            {forecast.period}
-                                        </Typography>
-                                        <Typography variant="body2">
-                                            {formatForecastDate(forecast.forecastDate)}
-                                        </Typography>
-                                    </Box>
-                                ))}
-                            </Box>
-                        </>
-                    )}
-                </Paper>
-            )}
+                    </>
+                )}
+            </Paper>
         </Box>
     );
 };
