56e4512dc899b7ceb4581112c3854c33dcd3ca76
Bayron <b.munoz15@ufromail.cl>
2024-10-10 15:11:10 -0300

    feat(validationTab): añade botones para cada organización y autorización en base a roles del usuario

--- src/component/early-warning-system/component/info-alert/ValidationTab.tsx
+++ src/component/early-warning-system/component/info-alert/ValidationTab.tsx
@@ -1,17 +1,40 @@
-import React from 'react';
-import { Box, Typography, Button } from '@mui/material';
-import theme from '../../../shared/theme/Theme'
-interface ValidationsTabProps {
+import React, { useState } from 'react';
+import { Box, Typography, Button, Grid } from '@mui/material';
+import theme from '../../../shared/theme/Theme';
+import { useAuth } from '../../../users/context/AuthenticationContext';
 
+interface ValidationsTabProps {
     dmcStatus: string;
     forecastDate: string;
     forecastTargetDate: string;
-
 }
-const ValidationsTab: React.FC<ValidationsTabProps> = ({ dmcStatus, forecastDate, forecastTargetDate }) => {
 
-    const handleValidation = () => {
-        alert('Alerta validada exitosamente.');
+const ValidationsTab: React.FC<ValidationsTabProps> = ({ dmcStatus, forecastDate, forecastTargetDate }) => {
+    const { authData } = useAuth();
+
+    const [sernageominValidated, setSernageominValidated] = useState(false);
+    const [sernageominValidationDate, setSernageominValidationDate] = useState<string | null>(null);
+    const [senapredValidated, setSenapredValidated] = useState(false);
+    const [senapredValidationDate, setSenapredValidationDate] = useState<string | null>(null);
+
+    const handleValidationToggle = (service: string) => {
+        const now = new Date().toLocaleString();
+
+        if (service === 'Sernageomin') {
+            if (!sernageominValidated) {
+                setSernageominValidationDate(now);
+            } else {
+                setSernageominValidationDate(now);
+            }
+            setSernageominValidated(!sernageominValidated);
+        } else if (service === 'Senapred') {
+            if (!senapredValidated) {
+                setSenapredValidationDate(now);
+            } else {
+                setSenapredValidationDate(now);
+            }
+            setSenapredValidated(!senapredValidated);
+        }
     };
 
     return (
@@ -29,55 +52,101 @@ const ValidationsTab: React.FC<ValidationsTabProps> = ({ dmcStatus, forecastDate
             </Typography>
             <Box
                 sx={{
-                    padding: '16px',
                     borderRadius: '8px',
                     maxWidth: '500px',
                     width: '100%',
                     textAlign: 'center',
+
                 }}
             >
-                <Box mb={2}>
-                    <Typography variant="body1">
-                        Estado DMC: <strong>{dmcStatus}</strong>
-                    </Typography>
-                </Box>
+                <Grid container >
+                    <Grid item xs={7}>
+                        <Typography variant="body1" sx={{ textAlign: 'left' }}>
+                            Estado DMC: <strong>{dmcStatus}</strong>
+                        </Typography>
+                    </Grid>
+                    <Grid item xs={5}></Grid>
+                    <Grid item xs={7} sx={{ textAlign: 'left' }}>
+                        <Typography variant="body1">
+                            Estado Sernageomin: <strong>{sernageominValidated ? 'Validado' : 'No validado'}</strong>
+                        </Typography>
+                        <Typography variant="body1">
+                            Fecha Validación: {sernageominValidationDate ?? 'N/A'}
+                        </Typography>
+                    </Grid>
+                    <Grid item xs={5} sx={{ display: 'flex', alignItems: 'center', justifyContent: 'center' }}>
+                        {authData?.role === 'SERNAGEOMIN' || authData?.role === 'ADMIN' ? (
+                            <Button
+                                variant="outlined"
+                                onClick={() => handleValidationToggle('Sernageomin')}
+                                sx={{
+                                    textTransform: 'none',
+                                    padding: '8px 16px',
+                                    fontWeight: 'bold',
+                                    color: sernageominValidated ? 'error.main' : 'success.main',
+                                    borderColor: sernageominValidated ? 'error.light' : 'success.light',
+                                    '&:hover': {
+                                        borderColor: sernageominValidated ? 'error.dark' : 'success.dark',
+                                    }
+                                }}
+                            >
+                                {sernageominValidated ? 'Quitar Validación' : 'Validar'}
+                            </Button>
+                        ) : null}
+                    </Grid>
+                    <Grid item xs={7} sx={{ textAlign: 'left' }}>
+                        <Typography variant="body1">
+                            Estado Senapred: <strong>{senapredValidated ? 'Validado' : 'No validado'}</strong>
+                        </Typography>
+                        <Typography variant="body1">
+                            Fecha Validación: {senapredValidationDate ?? 'N/A'}
+                        </Typography>
+                    </Grid>
+                    <Grid item xs={5} sx={{ display: 'flex', alignItems: 'center', justifyContent: 'center' }}>
+                        {authData?.role === 'SENAPRED' || authData?.role === 'ADMIN' ? (
+                            <Button
+                                variant="outlined"
+                                onClick={() => handleValidationToggle('Senapred')}
+                                sx={{
+                                    textTransform: 'none',
+                                    padding: '8px 16px',
+                                    fontWeight: 'bold',
+                                    color: senapredValidated ? 'error.main' : 'success.main',
+                                    borderColor: senapredValidated ? 'error.light' : 'success.light',
+                                    '&:hover': {
+                                        borderColor: senapredValidated ? 'error.dark' : 'success.dark',
+                                    }
+                                }}
+                            >
+                                {senapredValidated ? 'Quitar Validación' : 'Validar'}
+                            </Button>
+                        ) : null}
+                    </Grid>
+                </Grid>
+            </Box>
+            <Box
+                sx={{
 
-                <Box mb={2}>
-                    <Typography variant="body1">
-                        Estado Sernageomin: <strong>SIN validar</strong>
-                    </Typography>
-                </Box>
+                    borderRadius: '8px',
+                    maxWidth: '500px',
+                    width: '100%',
+                    textAlign: 'center',
+                    backgroundColor: theme.palette.grey[100],
+                }}
+            >
 
-                <Box mb={2}>
+                <Grid item xs={6}>
                     <Typography variant="body1">
                         Fecha de creación: <strong>{forecastDate}</strong>
                     </Typography>
-                </Box>
-                <Box mb={2}>
+                </Grid>
+                <Grid item xs={6}>
                     <Typography variant="body1">
                         Fecha de pronóstico: <strong>{forecastTargetDate}</strong>
                     </Typography>
-                </Box>
+                </Grid>
 
-                <Box mb={2}>
-                    <Typography variant="body1">Fecha de validación: <strong>None</strong></Typography>
 
-                </Box>
-
-                <Box textAlign="center" mt={3}>
-                    <Button
-                        variant="contained"
-                        color="primary"
-                        onClick={handleValidation}
-                        sx={{
-                            textTransform: 'none',
-                            padding: '8px 16px',
-                            fontWeight: 'bold',
-                        }}
-                    >
-                        Validar Alerta
-                    </Button>
-                </Box>
             </Box>
         </Box >
     );
